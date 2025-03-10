aace836ab436c1deca8ee41f0eab77a2c712e565
icedck <charm30072002@gmail.com>
2025-03-10 09:47:12 +0700

    add nhung

---
+++ nhungjs.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    document.getElementById("messageP").innerHTML = "Hello World";
+    document.write("Hello World");
+    console.log("Hello World");
+    alert("Hello World");
+}
